eaed1da4948957323bfb95fedd3fdf36bb484a52
Maxim <maxjust@inbox.ru>
2021-07-11 00:11:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace ConsoleClient
+{
+	internal sealed class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Title = "Client";
+
+			Console.Write($"Write hostname to connect: ");
+
+			var hostname = Console.ReadLine();
+
+			Console.Write($"Write port to connect: ");
+
+			var portString = Console.ReadLine();
+
+			Console.WriteLine(string.Empty);
+
+			if (!string.IsNullOrWhiteSpace(hostname))
+			{
+				if (!string.IsNullOrWhiteSpace(portString) && int.TryParse(portString, out var port))
+				{
+					try
+					{
+						while (true)
+						{
+							using (var tcpClient = new TcpClient())
+							{
+								Console.WriteLine($"Connecting to {hostname}:{port}...");
+
+								tcpClient.Connect(hostname, port);
+
+								if (tcpClient.Connected)
+								{
+									Console.WriteLine($"Connected");
+									Console.WriteLine(string.Empty);
+
+									Console.Write($"Your message: ");
+
+									var clientMessage = Console.ReadLine();
+
+									using (var networkStream = tcpClient.GetStream())
+									{
+										using (var streamWriter = new StreamWriter(networkStream) { AutoFlush = true })
+										{
+											streamWriter.WriteLine(clientMessage);
+
+											var serverMessage = string.Empty;
+
+											using (var streamReader = new StreamReader(networkStream))
+											{
+												serverMessage = streamReader.ReadLine();
+											}
+
+											Console.WriteLine($"Server message: {serverMessage}");
+										}
+									}
+
+									Console.WriteLine(string.Empty);
+								}
+								else
+									Console.WriteLine($"Not connected");
+							}
+						}
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex.Message);
+
+						File.WriteAllText("logs.bin", $"Message: {ex.Message}. StackTrace: {ex.StackTrace}");
+					}
+				}
+				else
+					Console.WriteLine($"Empty or incorrect port");
+			}
+			else
+				Console.WriteLine($"Empty hostname");
+
+			Console.ReadKey();
+		}
+	}
+}

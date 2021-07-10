fc15cee543a7ae06ba743017d639f43231f2fd9f
Maxim <maxjust@inbox.ru>
2021-07-11 00:16:05 +0300

    .

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,84 +1,68 @@
 ﻿using System;
 using System.IO;
+using System.Net;
 using System.Net.Sockets;
 
 namespace ConsoleClient
 {
 	internal sealed class Program
 	{
+		const string IP = "127.0.0.1";
+		const int PORT = 15000;
+
 		static void Main(string[] args)
 		{
 			Console.Title = "Client";
 
-			Console.Write($"Write hostname to connect: ");
-
-			var hostname = Console.ReadLine();
-
-			Console.Write($"Write port to connect: ");
-
-			var portString = Console.ReadLine();
-
-			Console.WriteLine(string.Empty);
-
-			if (!string.IsNullOrWhiteSpace(hostname))
+			try
 			{
-				if (!string.IsNullOrWhiteSpace(portString) && int.TryParse(portString, out var port))
+				while (true)
 				{
-					try
+					using (var tcpClient = new TcpClient())
 					{
-						while (true)
+						Console.WriteLine($"Connecting to {IP}:{PORT}...");
+
+						tcpClient.Connect(new IPEndPoint(IPAddress.Parse(IP), PORT));
+
+						if (tcpClient.Connected)
 						{
-							using (var tcpClient = new TcpClient())
+							Console.WriteLine($"Connected");
+							Console.WriteLine(string.Empty);
+
+							Console.Write($"Your message: ");
+
+							var clientMessage = Console.ReadLine();
+
+							using (var networkStream = tcpClient.GetStream())
 							{
-								Console.WriteLine($"Connecting to {hostname}:{port}...");
-
-								tcpClient.Connect(hostname, port);
-
-								if (tcpClient.Connected)
+								using (var streamWriter = new StreamWriter(networkStream) { AutoFlush = true })
 								{
-									Console.WriteLine($"Connected");
-									Console.WriteLine(string.Empty);
+									streamWriter.WriteLine(clientMessage);
 
-									Console.Write($"Your message: ");
+									var serverMessage = string.Empty;
 
-									var clientMessage = Console.ReadLine();
-
-									using (var networkStream = tcpClient.GetStream())
+									using (var streamReader = new StreamReader(networkStream))
 									{
-										using (var streamWriter = new StreamWriter(networkStream) { AutoFlush = true })
-										{
-											streamWriter.WriteLine(clientMessage);
-
-											var serverMessage = string.Empty;
-
-											using (var streamReader = new StreamReader(networkStream))
-											{
-												serverMessage = streamReader.ReadLine();
-											}
-
-											Console.WriteLine($"Server message: {serverMessage}");
-										}
+										serverMessage = streamReader.ReadLine();
 									}
 
-									Console.WriteLine(string.Empty);
+									Console.WriteLine($"Server message: {serverMessage}");
 								}
-								else
-									Console.WriteLine($"Not connected");
 							}
-						}
-					}
-					catch (Exception ex)
-					{
-						Console.WriteLine(ex.Message);
 
-						File.WriteAllText("logs.bin", $"Message: {ex.Message}. StackTrace: {ex.StackTrace}");
+							Console.WriteLine(string.Empty);
+						}
+						else
+							Console.WriteLine($"Not connected");
 					}
 				}
-				else
-					Console.WriteLine($"Empty or incorrect port");
 			}
-			else
-				Console.WriteLine($"Empty hostname");
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+
+				File.WriteAllText("logs.bin", $"Message: {ex.Message}. StackTrace: {ex.StackTrace}");
+			}
 
 			Console.ReadKey();
 		}
